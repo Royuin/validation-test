8e1d9155a46ad71f1d1a4b2ca034e4dd30ab5154
Gage <77414756+Royuin@users.noreply.github.com>
2023-04-05 22:43:26 -0500

    added functionality to show error message for when email is empty or is not a valid email and prevented submit if email is invalid

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ const zip = document.getElementById('zip');
 const pass = document.getElementById('pass');
 const passConfirm = document.getElementById('pass-confirm');
 const submit = document.getElementById('submit-button');
+const form = document.querySelector('form');
+
+const emailError = document.querySelector('.email-error');
+
+email.addEventListener('input', () => {
+  if (email.validity.typeMismatch) {
+    emailError.textContent = 'Must be an email address';
+    emailError.style.display = 'block';
+  } else if (email.validity.valueMissing) {
+    emailError.textContent = 'Must be filled out';
+  } else {
+    emailError.style.display = 'none';
+  }
+});
+
+form.addEventListener('submit', (e) => {
+  if (!email.validity.valid) {
+    e.preventDefault();
+    if (email.validity.valueMissing) {
+      emailError.textContent = 'Must be filled out';
+      emailError.style.display = 'block';
+    }
+  }
+});
