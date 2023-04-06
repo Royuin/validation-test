1e0cef9fdcd81a544cb6efacedf0897517abcda7
Gage <77414756+Royuin@users.noreply.github.com>
2023-04-05 20:21:23 -0500

    made selectors for all input elements and submit button

---
+++ script.js
@@ -0,0 +1,7 @@
+const email = document.getElementById('email');
+const countrySelect = document.getElementById('country');
+const country = countrySelect.value;
+const zip = document.getElementById('zip');
+const pass = document.getElementById('pass');
+const passConfirm = document.getElementById('pass-confirm');
+const submit = document.getElementById('submit-button');
