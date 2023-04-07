7bc2e24fd7488fb5734b31077ce814f69d9bc9df
Gage <77414756+Royuin@users.noreply.github.com>
2023-04-06 20:40:01 -0500

    added password validation

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ const form = document.querySelector('form');
 
 const emailError = document.querySelector('.email-error');
 const zipError = document.querySelector('.zip-error');
+const passError = document.querySelector('.pass-error');
+const passConfirmError = document.querySelector('.pass-confirm-error');
 
 email.addEventListener('input', (e) => {
   if (email.validity.typeMismatch) {
@@ -92,20 +94,71 @@ zip.addEventListener('input', (e) => {
   }
 });
 
+pass.addEventListener('input', () => {
+  const passConfirm = document.getElementById('pass-confirm');
+  if (pass.value !== passConfirm.value) {
+    passError.textContent = 'Passwords must match';
+    passError.style.display = 'block';
+    pass.setCustomValidity('invalid pass');
+  } else {
+    passError.style.display = 'none';
+    passConfirmError.style.display = 'none';
+    pass.setCustomValidity('');
+    passConfirm.setCustomValidity('');
+  }
+});
+
+passConfirm.addEventListener('input', () => {
+  const pass = document.getElementById('pass');
+  if (pass.value !== passConfirm.value) {
+    passConfirmError.textContent = 'Passwords must match';
+    passConfirmError.style.display = 'block';
+    passConfirm.setCustomValidity('invalid pass');
+  } else {
+    passConfirmError.style.display = 'none';
+    passError.style.display = 'none';
+    pass.setCustomValidity('');
+    passConfirm.setCustomValidity('');
+  }
+});
+
 form.addEventListener('submit', (e) => {
   const zip = document.getElementById('zip');
+  const pass = document.getElementById('pass');
+  const passConfirm = document.getElementById('pass-confirm');
+
   if (!email.validity.valid) {
     e.preventDefault();
     if (email.validity.valueMissing) {
       emailError.textContent = 'Must be filled out';
       emailError.style.display = 'block';
     }
-  } else if (zip.validity.valueMissing) {
+  }
+  if (zip.validity.valueMissing) {
     e.preventDefault();
     zip.setCustomValidity('Invalid zip.');
     zipError.textContent = 'Must be filled out';
     zipError.style.display = 'block';
   } else if (!zip.validity.valid) {
     e.preventDefault();
+  }
+  if (pass.validity.valueMissing) {
+    e.preventDefault();
+    passError.textContent = 'Must be filled out';
+    passError.style.display = 'block';
+  }
+  if (passConfirm.validity.valueMissing) {
+    e.preventDefault();
+    passConfirmError.textContent = 'Must be filled out';
+    passConfirmError.style.display = 'block';
+  }
+  if (pass.value !== passConfirm.value) {
+    e.preventDefault();
+    passConfirmError.textContent = 'Passwords must match';
+    passConfirmError.style.display = 'block';
+    passConfirm.setCustomValidity('invalid pass');
+    passError.textContent = 'Passwords must match';
+    passError.style.display = 'block';
+    pass.setCustomValidity('invalid pass');
   }
 });
